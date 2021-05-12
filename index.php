056f7a2f7b6dad1df5bb7d9fe2699b03cbce3784
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-05-11 17:42:33 -0700

    Prueba #?

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biodiversidad</title>
+    <title>Biodiversidad - Hackathon 2021</title>
 </head>
 <body>
-    <h1>Título de prueba uwu</h1>
+    <h1>Biodiversidad y medio ambiente</h1>
 </body>
 </html>

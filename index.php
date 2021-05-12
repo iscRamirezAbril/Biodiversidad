624c60e0ab34564a19af67127748509742b4f6c6
Abril Ramirez <isc.ramirez_abrilk@outlook.com>
2021-05-11 17:49:34 -0700

    Se agregaron títulos de prueba

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biodiversidad</title>
+    <title>Biodiversidad - Hackathon 2021</title>
 </head>
 <body>
-    <h1>Título de prueba uwu</h1>
+    <h1>Biodiversidad y medio ambiente</h1>
+    <h2>Biodiversidad</h2>
 </body>
 </html>

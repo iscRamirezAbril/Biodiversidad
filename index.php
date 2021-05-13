7a41b574bde1ffce47775ef48d87eea755977f35
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-05-12 20:52:15 -0700

    Nuevas fuentes

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <h2>¿Qué es la biodiversidad?</h2>
     <p>
        La "Biodiversidad", también conocida como "Diversidad Biológica, comprende todas las formas de vida existentes,
-       es decir, la variabilidad de organismos vivos Abarca la diversidad de especies de </p>
+       es decir, la variabilidad de organismos vivos. Abarca la diversidad de especies de </p>
        <li> Plantas </li>
        <li> Animales </li>
        <li> Hongos </li>

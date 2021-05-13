c6a7574c83754692d223ab6a4f437a14529e7509
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-05-12 21:07:29 -0700

    Nuevas fuentes

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biodiversidad - Hackathon 2021</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital@1&display=swap" rel="stylesheet">
 </head>
 <body>
-    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
     <h1>Biodiversidad y medio ambiente</h1>
     <h2>¿Qué es la biodiversidad?</h2>
     <p>

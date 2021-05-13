a71254b3a3fde77060f11511dc4437f2e2107f08
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-05-13 14:50:57 -0700

    Última modificación

--- index.php
+++ index.php
@@ -69,8 +69,8 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-md-20 col-lg-8 mx-auto">
-                ¡Siguenos en nuestras redes sociales!
+                <div class="col-md-10 col-lg-8 mx-auto">
+                <p class="text-center">¡Siguenos en nuestras redes sociales!</p> 
                     <ul class="list-inline text-center">
                         <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></li>
                         <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></li>

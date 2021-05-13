1c44273004a05bf4e3030ac303383a519984e8b1
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-05-12 21:45:33 -0700

    Nuevo color de fuente

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,23 +8,26 @@
     <title>Biodiversidad - Hackathon 2021</title>
     <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital@1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital@1&display=swap" rel="stylesheet">
 </head>
 <header>
-<nav></nav>
+    <nav></nav>
 </header>
+
 <body>
-<div id="Concepto">    
-    <h1>Biodiversidad y medio ambiente</h1>
-    <h2>¿Qué es la biodiversidad?</h2>
-    <p>
-       La "Biodiversidad", también conocida como "Diversidad Biológica, comprende todas las formas de vida existentes,
-       es decir, la variabilidad de organismos vivos. Abarca la diversidad de especies de </p>
-       <li> Plantas </li>
-       <li> Animales </li>
-       <li> Hongos </li>
-       <li> Microorganismos </li>
-    <p>que viven en un espacio deterinado</p></div>
+    <div id="Concepto">
+        <h1>Biodiversidad y medio ambiente</h1>
+        <h2>¿Qué es la biodiversidad?</h2>
+        <p>
+            La "Biodiversidad", también conocida como "Diversidad Biológica, comprende todas las formas de vida existentes,
+            es decir, la variabilidad de organismos vivos. Abarca la diversidad de especies de </p>
+        <li> Plantas </li>
+        <li> Animales </li>
+        <li> Hongos </li>
+        <li> Microorganismos </li>
+        <p>que viven en un espacio deterinado</p>
+    </div>
 </body>
 <footer></footer>
+
 </html>
